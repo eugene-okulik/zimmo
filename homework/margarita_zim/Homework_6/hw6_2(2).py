a6598c0a6656ea2ccb6dd2c06dc5aacc3f79b411
Margarita Zimodro <zimmo@mail.ru>
2026-05-31 18:49:03 +0700

    Lesson_6_corrections2

--- homework/margarita_zim/Homework_6/hw6_2(2).py
+++ homework/margarita_zim/Homework_6/hw6_2(2).py
@@ -1,11 +1,11 @@
 # способ 2
 digits = list(range(1, 101))
 for digit in digits:
-      if digit % 15 == 0:
-          print("FuzzBuzz")
-      elif digit % 3 == 0:
-          print("Fuzz")
-      elif digit % 5 == 0:
-          print("Buzz")
-      else:
-          print(digit)
+    if digit % 15 == 0:
+        print("FuzzBuzz")
+    elif digit % 3 == 0:
+        print("Fuzz")
+    elif digit % 5 == 0:
+        print("Buzz")
+    else:
+        print(digit)
