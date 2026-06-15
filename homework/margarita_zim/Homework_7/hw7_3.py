b602d6e6e3daf243b5e16e62d7dd4245c49f9791
Margarita Zimodro <zimmo@mail.ru>
2026-06-15 11:25:24 +0700

    Homework_7

---
+++ homework/margarita_zim/Homework_7/hw7_3.py
@@ -0,0 +1,15 @@
+def add_10_to_num(text):
+  dv = text.index(': ')
+  num_str = text[dv + 2:]
+  return int(num_str) + 10
+
+
+results = [
+  'результат операции: 42',
+  'результат операции: 54',
+  'результат работы программы: 209',
+  'результат: 2'
+]
+
+for res in results:
+  print(add_10_to_num(res))
